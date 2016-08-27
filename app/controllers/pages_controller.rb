aa8f7c344aea92e64687081d81ba14b924a6c468
stormysunshine <jenniferchaiken@yahoo.com>
2016-08-27 23:33:35 +0000

    update home page with account info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
     def home
-    
+        @accounts = Account.all
+        @cash = Account.find_by(account_type: 'cash')
+        @bank = Account.find_by(account_type: 'bank')
     end
 
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
 class Account < ActiveRecord::Base
+    
+    validates :name, presence: true
+    validates :balance, presence: true
+    validates :account_type, presence: true
+    
 end
